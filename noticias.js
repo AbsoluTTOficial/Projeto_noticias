591feb8103af7f425414b3bb2e77b9f093d09de8
Vinicius Cezario Rodrigues <vcroficial.07@gmail.com>
2021-06-23 12:39:50 -0300

    section-2
    Respondendo requisições com base na url requisitada

--- noticias.js
+++ noticias.js
@@ -1,7 +1,23 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-    res.end("<html><body>Portal de notícias</body></html>");
+
+    var categoria = req.url;
+
+    if(categoria == '/tecnologia'){
+        res.end("<html><body><h1>Noticias de Tecnologia</h1></body></html>");
+
+    } else if (categoria == '/moda'){
+        res.end("<html><body><h1>Noticias de Moda</h1></body></html>");
+        
+    } else if (categoria == '/beleza'){
+        res.end("<html><body><h1>Noticias de Beleza</h1></body></html>");
+
+    } else {
+        res.end("<html><body><h1>Portal de noticias</h1></body></html>");
+
+    }
+    
 })
 
 server.listen(3000);
