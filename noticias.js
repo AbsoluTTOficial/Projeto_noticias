bf2251efb96648a86337769f28d6b382ee962d69
Vinicius Cezario Rodrigues <vcroficial.07@gmail.com>
2021-06-23 12:17:54 -0300

    Respondendo-requisições-HTTP

--- noticias.js
+++ noticias.js
@@ -1,1 +1,7 @@
-console.log('Hello World!')
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    res.end("<html><body>Portal de notícias</body></html>");
+})
+
+server.listen(3000);
