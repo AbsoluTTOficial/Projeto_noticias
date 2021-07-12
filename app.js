a174ffd1d22df12b475ec4f6b4447ff0cd946f10
Vinicius Cezario Rodrigues <vcroficial.07@gmail.com>
2021-07-12 12:51:56 -0300

    criando módulos de definição de rotas

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 var app = require('./config/server');
 
-app.get('/', function(req, res){
-    res.render("home/index.ejs")
-});
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
-app.get('/formulario_inclusao_noticia', function(req, res){
-    res.render("admin/form_add_noticia.ejs")
-});
-
-app.get('/noticias', function(req, res){
-    res.render("noticias/noticias.ejs")
-});
 
 app.listen(3000, function(){
     console.log('Servidor ON');
