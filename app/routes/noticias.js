86bf2232ca19cfd4452fa4712c3acdb350674a6d
Vinicius Cezario Rodrigues <vcroficial.07@gmail.com>
2021-08-03 11:24:47 -0300

    conexão mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,21 @@
 module.exports = function(app){
+
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias.ejs")
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'vcri007.',
+            database : 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function(error, result){
+                res.send(result);
+            });
+
+
+        //res.render("noticias/noticias.ejs")
     });
 };
