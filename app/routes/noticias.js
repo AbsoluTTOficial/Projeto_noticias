fc719cf842106c5a21ed034539eaab0b1d463cca
Vinicius Cezario Rodrigues <vcroficial.07@gmail.com>
2021-08-05 11:51:05 -0300

    refactoring
    refatorando o método de conexão com o banco de dados mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,19 +1,13 @@
+var dbConnection = require('../../config/dbConnection.js');
+
 module.exports = function(app){
+    
+    var connection = dbConnection();
+
     app.get('/noticias', function(req, res){
-
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : 'vcri007.',
-            database : 'portal_noticias'
-
-        });
 
         connection.query('select * from noticias' ,function (error, result){
             res.render("noticias/noticias", {noticias : result});
         });
-
     });
 };
